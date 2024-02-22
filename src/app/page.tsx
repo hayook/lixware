02f592eeb64a25a9a756ca297c0e1517086dffe2
hayook <hay.eddine8@gmail.com>
2024-02-22 08:50:40 +0100

    added: Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-	return <h1>Lixware</h1>
+import Link from "next/link";
+import { sqlite3 } from "@/lib/sqlite3";
+import styles from "./styles.module.css";
+
+const sql = "SELECT word_id, word_label FROM words;";
+
+export default async function Home() {
+
+	// Connect to the database 
+	const db = await sqlite3(process.env.DB as string);
+
+	// Get all words from the words table
+	const words = await db.all(sql);
+
+	return (
+		<>
+			<div className={`container ${styles.homeContainer}`}>
+				{
+					words.map(word => {
+						return (
+							<Link
+								key={word.word_id}
+								href={`/words/${word.word_id}`}
+								className={styles.wordCard}
+							>
+								{word.word_label}
+							</Link>
+						)
+					})
+				}
+			</div>
+			<Link className={styles.addWord} href="/new"><span>+</span></Link>
+		</>
+	);
 }
